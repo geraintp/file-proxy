bd97cf85072d91b1edf3e8d0cc0227e8cbbe7786
geraint <geraint@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-10 16:48:42 +0000

    git-svn-id: http://plugins.svn.wordpress.org/file-proxy/trunk@215853 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- com/twothirdsdesign/file-proxy/admin/admin.php
+++ com/twothirdsdesign/file-proxy/admin/admin.php
@@ -106,6 +106,7 @@ class TtdFileProxyAdmin
 	function enqueue_script() {
 		$src = TTDFP_URL .'assets/js/iphone-style-checkboxes.js';
 		wp_enqueue_script("iphone-style-checkboxes" , $src, "jquery", $this->m->get_option("version"), false );
+
 	}
 	
 	/**
@@ -130,8 +131,224 @@ function editUrlKey(){
 }
 //]]>
 </script> 
-	<?php
-	}
+<?php 
+//AJAX Upload
+?>
+<script type="text/javascript" src="<?php echo get_bloginfo('template_directory'); ?>/functions/js/ajaxupload.js"></script>
+<script type="text/javascript">
+    jQuery(document).ready(function(){
+    
+    var flip = 0;
+        
+    jQuery('#expand_options').click(function(){
+        if(flip == 0){
+            flip = 1;
+            jQuery('#ttd_file_proxy_container #ttd-nav').hide();
+            jQuery('#ttd_file_proxy_container #content').width(755);
+            jQuery('#ttd_file_proxy_container .group').add('#ttd_file_proxy_container .group h2').show();
+
+            jQuery(this).text('[-]');
+            
+        } else {
+            flip = 0;
+            jQuery('#ttd_file_proxy_container #ttd-nav').show();
+            jQuery('#ttd_file_proxy_container #content').width(595);
+            jQuery('#ttd_file_proxy_container .group').add('#ttd_file_proxy_container .group h2').hide();
+            jQuery('#ttd_file_proxy_container .group:first').show();
+            jQuery('#ttd_file_proxy_container #ttd-nav li').removeClass('current');
+            jQuery('#ttd_file_proxy_container #ttd-nav li:first').addClass('current');
+            
+            jQuery(this).text('[+]');
+        
+        }
+    
+    });
+    
+        jQuery('.group').hide();
+        jQuery('.group:first').fadeIn();
+        jQuery('.ttd-radio-img-img').click(function(){
+            jQuery(this).parent().parent().find('.ttd-radio-img-img').removeClass('ttd-radio-img-selected');
+            jQuery(this).addClass('ttd-radio-img-selected');
+            
+        });
+        jQuery('.ttd-radio-img-label').hide();
+        jQuery('.ttd-radio-img-img').show();
+        jQuery('.ttd-radio-img-radio').hide();
+        jQuery('#ttd-nav li:first').addClass('current');
+        jQuery('#ttd-nav li a').click(function(evt){
+        
+                jQuery('#ttd-nav li').removeClass('current');
+                jQuery(this).parent().addClass('current');
+                
+                var clicked_group = jQuery(this).attr('href');
+ 
+                jQuery('.group').hide();
+                
+                    jQuery(clicked_group).fadeIn();
+
+                evt.preventDefault();
+                
+            });
+        
+        if('<?php if(isset($_REQUEST['reset'])) { echo $_REQUEST['reset'];} else { echo 'false';} ?>' == 'true'){
+            
+            var reset_popup = jQuery('#ttd-popup-reset');
+            reset_popup.fadeIn();
+            window.setTimeout(function(){
+                   reset_popup.fadeOut();                        
+                }, 2000);
+                //alert(response);
+            
+        }
+            
+    //Update Message popup
+    jQuery.fn.center = function () {
+        this.animate({"top":( jQuery(window).height() - this.height() - 200 ) / 2+jQuery(window).scrollTop() + "px"},100);
+        this.css("left", 250 );
+        return this;
+    }
+
+    
+    jQuery('#ttd-popup-save').center();
+    jQuery('#ttd-popup-reset').center();
+    jQuery(window).scroll(function() { 
+    
+        jQuery('#ttd-popup-save').center();
+        jQuery('#ttd-popup-reset').center();
+    
+    });
+    
+    
+
+    //AJAX Upload
+    jQuery('.image_upload_button').each(function(){
+    
+    var clickedObject = jQuery(this);
+    var clickedID = jQuery(this).attr('id');	
+    new AjaxUpload(clickedID, {
+          action: '<?php echo admin_url("admin-ajax.php"); ?>',
+          name: clickedID, // File upload name
+          data: { // Additional data to send
+                action: 'ttd_ajax_post_action',
+                type: 'upload',
+                data: clickedID },
+          autoSubmit: true, // Submit file after selection
+          responseType: false,
+          onChange: function(file, extension){},
+          onSubmit: function(file, extension){
+                clickedObject.text('Uploading'); // change button text, when user selects file	
+                this.disable(); // If you want to allow uploading only 1 file at time, you can disable upload button
+                interval = window.setInterval(function(){
+                    var text = clickedObject.text();
+                    if (text.length < 13){	clickedObject.text(text + '.'); }
+                    else { clickedObject.text('Uploading'); } 
+                }, 200);
+          },
+          onComplete: function(file, response) {
+           
+            window.clearInterval(interval);
+            clickedObject.text('Upload Image');	
+            this.enable(); // enable upload button
+            
+            // If there was an error
+            if(response.search('Upload Error') > -1){
+                var buildReturn = '<span class="upload-error">' + response + '</span>';
+                jQuery(".upload-error").remove();
+                clickedObject.parent().after(buildReturn);
+            
+            }
+            else{
+                var buildReturn = '<img class="hide ttd-option-image" id="image_'+clickedID+'" src="'+response+'" width="300" alt="" />';
+//					var buildReturn = '<img class="hide" id="image_'+clickedID+'" src="<?php bloginfo('template_url') ?>/thumb.php?src='+response+'&w=345" alt="" />';
+                jQuery(".upload-error").remove();
+                jQuery("#image_" + clickedID).remove();	
+                clickedObject.parent().after(buildReturn);
+                jQuery('img#image_'+clickedID).fadeIn();
+                clickedObject.next('span').fadeIn();
+                clickedObject.parent().prev('input').val(response);
+            }
+          }
+        });
+    
+    });
+    
+    //AJAX Remove (clear option value)
+    jQuery('.image_reset_button').click(function(){
+    
+            var clickedObject = jQuery(this);
+            var clickedID = jQuery(this).attr('id');
+            var theID = jQuery(this).attr('title');	
+
+            var ajax_url = '<?php echo admin_url("admin-ajax.php"); ?>';
+        
+            var data = {
+                action: 'ttd_ajax_post_action',
+                type: 'image_reset',
+                data: theID
+            };
+            
+            jQuery.post(ajax_url, data, function(response) {
+                var image_to_remove = jQuery('#image_' + theID);
+                var button_to_hide = jQuery('#reset_' + theID);
+                image_to_remove.fadeOut(500,function(){ jQuery(this).remove(); });
+                button_to_hide.fadeOut();
+                clickedObject.parent().prev('input').val('');
+                
+                
+                
+            });
+            
+            return false; 
+            
+        });   	 	
+
+
+
+    //Save everything else
+    jQuery('#ttdform').submit(function(){
+        
+            function newValues() {
+              var serializedValues = jQuery("#ttdform").serialize();
+              return serializedValues;
+            }
+            jQuery(":checkbox, :radio").click(newValues);
+            jQuery("select").change(newValues);
+            jQuery('.ajax-loading-img').fadeIn();
+            var serializedReturn = newValues();
+             
+            var ajax_url = '<?php echo admin_url("admin-ajax.php"); ?>';
+        
+             //var data = {data : serializedReturn};
+            var data = {
+                <?php if(isset($_REQUEST['page']) && $_REQUEST['page'] == 'ttdthemes_framework_settings'){ ?>
+                type: 'framework',
+                <?php } ?>
+                action: 'ttd_ajax_post_action',
+                data: serializedReturn
+            };
+            
+            jQuery.post(ajax_url, data, function(response) {
+                var success = jQuery('#ttd-popup-save');
+                var loading = jQuery('.ajax-loading-img');
+                loading.fadeOut();  
+                success.fadeIn();
+                window.setTimeout(function(){
+                   success.fadeOut(); 
+                   
+                                        
+                }, 2000);
+            });
+            
+            return false; 
+            
+        });   	 	
+        
+    });
+</script>
+
+<?php 
+}
+
    
 	/**
 	 * Creates the default meta boxes for the theme settings page. Child theme and plugin developers
@@ -188,6 +405,39 @@ function editUrlKey(){
 		}
 	}
 
+	function admin_header(){ ?>
+        <div class="wrap" id="ttd_file_proxy_container">
+        <div id="ttd-popup-save" class="ttd-save-popup"><div class="ttd-save-save">Options Updated</div></div>
+        <div id="ttd-popup-reset" class="ttd-save-popup"><div class="ttd-save-reset">Options Reset</div></div>
+        <?php // <form method="post"  enctype="multipart/form-data"> ?>
+        <form action="" enctype="multipart/form-data" id="ttdform">
+            <div id="header">
+                <div class="logo"><img alt="ttdThemes" src="http://c0392561.cdn.cloudfiles.rackspacecloud.com/plugin-logo.png"/></div>
+                <div class="theme-info">
+                    <span class="theme">File-Proxy<?php echo $themename; ?> <?php echo $local_version; ?></span>
+                    <span class="framework">version <?php echo $this->m->get_option("version", 0 ); ?></span>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div id="support-links">
+       
+                <ul>
+                    <li class="changelog"><a title="Theme Changelog" href="<?php echo $manualurl; ?>#Changelog">View Changelog</a></li>
+                    <li class="docs"><a title="Theme Documentation" href="<?php echo $manualurl; ?>">View Themedocs</a></li>
+                    <li class="forum"><a href="http://forum.ttdthemes.com" target="blank">Visit Forum</a></li>
+                    <li class="right"><img style="display:none" src="<?php echo bloginfo('template_url'); ?>/functions/images/loading-top.gif" class="ajax-loading-img ajax-loading-img-top" alt="Working..." /><a href="#" id="expand_options" class='hide-if-no-js'>[+]</a> <input type="submit" value="Save All Changes" class="button submit-button" /></li>
+                </ul>
+       
+            </div><?php 
+	}
+	
+	function admin_footer(){ ?>
+    
+    		<div style="clear:both;"></div>   
+		</div><!--wrap--><?php
+	}
+	
+	
 	/**
 	 * Displays the plugin settings page and calls do_meta_boxes() to allow additional settings
 	 * meta boxes to be added to the page.
@@ -196,7 +446,10 @@ function editUrlKey(){
 	 */
 	function render_settings_page() {
 		
+		$this->admin_header();
 		require_once 'settings_page.php';
+		$this->admin_footer();
+		
 		if(false) {?>
 		<div class="wrap">
 		<div id="icon-options-general" class="icon32"><br /></div> 
--- com/twothirdsdesign/file-proxy/admin/settings_page.php
+++ com/twothirdsdesign/file-proxy/admin/settings_page.php
@@ -1,29 +1,6 @@
-<div class="wrap" id="woo_container">
-<div id="woo-popup-save" class="woo-save-popup"><div class="woo-save-save">Options Updated</div></div>
-<div id="woo-popup-reset" class="woo-save-popup"><div class="woo-save-reset">Options Reset</div></div>
-    <?php // <form method="post"  enctype="multipart/form-data"> ?>
-    <form action="" enctype="multipart/form-data" id="wooform">
-        <div id="header">
-            <div class="logo"><img alt="WooThemes" src="<?php echo bloginfo('template_url'); ?>/functions/images/logo.png"/></div>
-            <div class="theme-info">
-                <span class="theme"><?php echo $themename; ?> <?php echo $local_version; ?></span>
-                <span class="framework">Framework <?php echo get_option('woo_framework_version'); ?></span>
-            </div>
-            <div class="clear"></div>
-        </div>
-        <div id="support-links">
-   
-            <ul>
-                <li class="changelog"><a title="Theme Changelog" href="<?php echo $manualurl; ?>#Changelog">View Changelog</a></li>
-                <li class="docs"><a title="Theme Documentation" href="<?php echo $manualurl; ?>">View Themedocs</a></li>
-                <li class="forum"><a href="http://forum.woothemes.com" target="blank">Visit Forum</a></li>
-                <li class="right"><img style="display:none" src="<?php echo bloginfo('template_url'); ?>/functions/images/loading-top.gif" class="ajax-loading-img ajax-loading-img-top" alt="Working..." /><a href="#" id="expand_options">[+]</a> <input type="submit" value="Save All Changes" class="button submit-button" /></li>
-            </ul>
-   
-        </div>
        
         <div id="main">
-            <div id="woo-nav">
+            <div id="ttd-nav" class="hide-if-no-js">
                 <ul>
                     <li class="current"><a href="#generaloptions">General Options</a></li>
                     <li class=""><a href="#importoptions">Import Options</a></li>
@@ -36,12 +13,12 @@
                     <div class="section section-checkbox">
                         <h3 class="heading">Disable options panel ouput option</h3>
                         <div class="option">
-                            <div class="controls">
-                            <input id="woo_show_options" name="woo_show_options" class="checkbox woo-input" type="checkbox" value="true" <?php if(get_option('woo_show_options') == 'true' ) { echo 'checked=""'; } ?>/>
+                            <div class="controls on_off">
+                            <input id="ttd_show_options" name="ttd_show_options" class="checkbox ttd-input" type="checkbox" value="true" <?php if(get_option('ttd_show_options') == 'true' ) { echo 'checked=""'; } ?>/>
                             <br/>
                             </div>
                             <div class="explain">
-                                Disable the ability to show your Woo Options. You can view the themes option by URL e.g. http://yoursite.com/?options=woo
+                                Disable the ability to show your ttd Options. You can view the themes option by URL e.g. http://yoursite.com/?options=ttd
                             </div>
                             <div class="clear"></div>
                           </div>
@@ -50,8 +27,8 @@
                        <div class="section section-checkbox">
                         <h3 class="heading">Theme Version Checker</h3>
                        <div class="option">
-                            <div class="controls">
-                            <input id="woo_theme_version_checker" name="woo_theme_version_checker" class="checkbox woo-input" type="checkbox" value="true" <?php if(get_option('woo_theme_version_checker') == 'true' ) { echo 'checked=""'; } ?>/>
+                            <div class="controls on_off danger">
+                            <input id="ttd_theme_version_checker" name="ttd_theme_version_checker" class="checkbox ttd-input" type="checkbox" value="true" <?php if(get_option('ttd_theme_version_checker') == 'true' ) { echo 'checked=""'; } ?>/>
                             <br/>
                             </div>
                             <div class="explain">
@@ -64,11 +41,11 @@
                         <h3 class="heading">Disable Buy Themes Tab</h3>
                        <div class="option">
                             <div class="controls">
-                            <input id="woo_buy_themes" name="woo_buy_themes" class="checkbox woo-input" type="checkbox" value="true" <?php if(get_option('woo_buy_themes') == 'true' ) { echo 'checked=""'; } ?>/>
+                            <input id="ttd_buy_themes" name="ttd_buy_themes" class="checkbox ttd-input" type="checkbox" value="true" <?php if(get_option('ttd_buy_themes') == 'true' ) { echo 'checked=""'; } ?>/>
                             <br/>
                             </div>
                             <div class="explain">
-                                This disables the "Buy Themes" tab. This page lists latest availbe themes from the WooThemes.com website.
+                                This disables the "Buy Themes" tab. This page lists latest availbe themes from the ttdThemes.com website.
                             </div>
                             <div class="clear"></div>
                          </div>
@@ -77,11 +54,11 @@
                         <h3 class="heading">Framework Core update (BETA)</h3>
                        <div class="option">
                             <div class="controls">
-                            <input id="woo_framework_update" name="woo_framework_update" class="checkbox woo-input" type="checkbox" value="true" <?php if(get_option('woo_framework_update') == 'true' ) { echo 'checked=""'; } ?>/>
+                            <input id="ttd_framework_update" name="ttd_framework_update" class="checkbox ttd-input" type="checkbox" value="true" <?php if(get_option('ttd_framework_update') == 'true' ) { echo 'checked=""'; } ?>/>
                             <br/>
                             </div>
                             <div class="explain">
-                                <strong>BETA:</strong> This option will active the WooFramework core update. Please use with caution. If you get an error when going to the Framework Update page please ensure you have added <code>require_once ($functions_path . 'admin-framework-update.php');</code> to your theme's <code>functions.php</code> file.
+                                <strong>BETA:</strong> This option will active the ttdFramework core update. Please use with caution. If you get an error when going to the Framework Update page please ensure you have added <code>require_once ($functions_path . 'admin-framework-update.php');</code> to your theme's <code>functions.php</code> file.
                             </div>
                             <div class="clear"></div>
                             </div>
@@ -91,14 +68,14 @@
                  <div id="importoptions" class="group" style="display: block;">
                     <h2>Import Options</h2>
                     <div class="section">
-                        <h3 class="heading">Import options from another WooThemes instance.</h3>
+                        <h3 class="heading">Import options from another ttdThemes instance.</h3>
                         <div class="option">
                             <div class="controls">
-                            <textarea rows="8" cols="" id="woo_import_options" name="woo_import_options" class="woo-input"></textarea>
+                            <textarea rows="8" cols="" id="ttd_import_options" name="ttd_import_options" class="ttd-input"></textarea>
                             <br/>
                             </div>
                             <div class="explain">
-                                You can transfer options from another WooThemes (same theme) to this one by copying the export code and adding it here. Works best if it's imported from identical themes.
+                                You can transfer options from another ttdThemes (same theme) to this one by copying the export code and adding it here. Works best if it's imported from identical themes.
                             </div>
                             <div class="clear"></div>
                             </div>
@@ -113,16 +90,16 @@
                             <?php
                             //Create, Encrypt and Update the Saved Settings
                             global $wpdb;
-                            $query = "SELECT * FROM $wpdb->options WHERE option_name LIKE 'woo_%' AND
-                                        option_name != 'woo_options' AND
-                                        option_name != 'woo_template' AND
-                                        option_name != 'woo_custom_template' AND
-                                        option_name != 'woo_settings_encode' AND
-                                        option_name != 'woo_export_options' AND
-                                        option_name != 'woo_import_options' AND
-                                        option_name != 'woo_framework_version' AND
-                                        option_name != 'woo_manual' AND
-                                        option_name != 'woo_shortname'";
+                            $query = "SELECT * FROM $wpdb->options WHERE option_name LIKE 'ttd_%' AND
+                                        option_name != 'ttd_options' AND
+                                        option_name != 'ttd_template' AND
+                                        option_name != 'ttd_custom_template' AND
+                                        option_name != 'ttd_settings_encode' AND
+                                        option_name != 'ttd_export_options' AND
+                                        option_name != 'ttd_import_options' AND
+                                        option_name != 'ttd_framework_version' AND
+                                        option_name != 'ttd_manual' AND
+                                        option_name != 'ttd_shortname'";
                            
                             $results = $wpdb->get_results($query);
                            
@@ -133,11 +110,11 @@
                             }
                             $output = serialize($output);
                             ?>
-                            <textarea rows="8" cols="" class="woo-input"><?php echo base64_encode($output); ?></textarea>
+                            <textarea rows="8" cols="" class="ttd-input"><?php echo base64_encode($output); ?></textarea>
                             <br/>
                             </div>
                             <div class="explain">
-                                You can transfer options from another WooThemes (same theme) to this one by copying the export code and adding it here. Works best if it's imported from identical themes.
+                                You can transfer options from another ttdThemes (same theme) to this one by copying the export code and adding it here. Works best if it's imported from identical themes.
                             </div>
                             <div class="clear"></div>
                             </div>
@@ -153,11 +130,11 @@
         <input type="submit" value="Save All Changes" class="button submit-button" />       
         </form>
          <?php /*
-        <form action="<?php echo wp_specialchars( $_SERVER['REQUEST_URI'] ) ?>" method="post" style="display:inline" id="wooform-reset">
+        <form action="<?php echo wp_specialchars( $_SERVER['REQUEST_URI'] ) ?>" method="post" style="display:inline" id="ttdform-reset">
             <span class="submit-footer-reset">
 
             <input name="reset" type="submit" value="Reset Options" class="button submit-button reset-button" onclick="return confirm('Click OK to reset. Any settings will be lost!');" />
-            <input type="hidden" name="woo_save" value="reset" />
+            <input type="hidden" name="ttd_save" value="reset" />
             </span>
         </form>
         */ ?>
@@ -165,7 +142,3 @@
         </div>
         <?php  // echo $update_message; ?>   
         <?php  //wp_nonce_field('reset_options'); echo "\n"; // Legacy ?>
-
-
-<div style="clear:both;"></div>   
-</div><!--wrap-->
